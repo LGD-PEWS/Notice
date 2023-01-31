0b6bfe75b2bad9395a105b768a6c8a2e9fa50169
sylvie <1553902582@qq.com>
2023-01-31 16:45:01 +0800

    ’将token提出来‘

--- send.py
+++ send.py
@@ -1,9 +1,9 @@
 import requests
-from variable import title1
+from variable import title1, token1
 
 
 def send_notice(content):
-    token = "5cc97d72996d42e9a884c69167fa6310"
+    token = token1
     title = title1
     url = f"http://www.pushplus.plus/send?token={token}&title={title}&content={content}&template=html"
     response = requests.request("GET", url)
--- variable.py
+++ variable.py
@@ -2,6 +2,8 @@
 path = r"C:\Users\Sylvie\Desktop\test\python\test.txt"
 # 监测的频率 （单位：秒）
 frequency = 1
+# 这里放token
+token1 = '这里放token'
 # 在这里修改标题
 title1 = '文件改变了'
 # 在这里修改内容
